26ba1108fa77dfb44809accf53f529104e817a6e
Brian Grant <brian@skeletonjack.net>
2023-04-23 06:08:37 -0500

    solved

--- test/unit/test_thirteen.py
+++ test/unit/test_thirteen.py
@@ -1,12 +1,12 @@
-from py_euler import thirteen
+from py_euler import thirteen as test_me
 
 ANSWER = "5537376230"
 TOTAL = "5537376230390876637302048746832985971773659831892672"
 
 
 def test_sum_numbers() -> None:
-    assert TOTAL == thirteen.sum_numbers(thirteen.NUMBERS)
+    assert TOTAL == test_me.sum_numbers(test_me.NUMBERS)
 
 
 def test_solve_problem() -> None:
-    assert ANSWER == thirteen.solve_problem(thirteen.NUMBERS)
+    assert ANSWER == test_me.solve_problem(test_me.NUMBERS)
